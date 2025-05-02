a2d60f8c3e75fcab1a9197edda28cef9e3fac551
Богдан <brazbogdan@gmail.com>
2025-05-02 20:09:16 +0300

    updated commit

--- flower_shop/src/components/Header/Header.jsx
+++ flower_shop/src/components/Header/Header.jsx
@@ -3,18 +3,15 @@ import styles from './Header.module.css';
 import Nav from './Nav/Nav.jsx';
 import ControlPanel from './ControlPanel/ControlPanel.jsx';
 
-
 export default function Header() {
   return (
     <header className={styles.header}>
-    <div className={styles.headerContainer}>
-   
-      
-    <Nav></Nav>
-    </div>
-    <ControlPanel></ControlPanel>
-   
+      <div className={styles.headerContainer}>
+        <div className={styles.navigationPanel}>
+          <Nav />
+          <ControlPanel />
+        </div>
+      </div>
     </header>
-    
-  )
+  );
 }
