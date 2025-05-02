c155c685f289e2d0604ad09231597e64da75cc39
Богдан <brazbogdan@gmail.com>
2025-05-02 13:42:54 +0300

    updated commit

--- flower_shop/src/components/Main/Main.jsx
+++ flower_shop/src/components/Main/Main.jsx
@@ -5,8 +5,21 @@ export default function Main() {
   return (
     <section className= {styles.mainBanner}>
         <div className={styles.logoContainer}>
-        <img src="fon/logo.svg" alt="logo" />
+        <img src="fon/logo.svg" alt="logo" className= {styles.logo} />
         </div>
+        <ul className= {styles.socialList}>
+            <li className= {styles.socailListItem}>
+                <button className= {styles.buttonItem}>
+                <img src="social/insta.svg" alt="instagram" className= {styles.buttonIcon} />
+                </button>
+            </li>
+            <li className= {styles.socailListItem}>
+            <button className= {styles.buttonItem}></button>
+            </li>
+            <li className= {styles.socailListItem}>
+            <button className= {styles.buttonItem}></button>
+            </li>
+        </ul>
     </section>
   )
 }
