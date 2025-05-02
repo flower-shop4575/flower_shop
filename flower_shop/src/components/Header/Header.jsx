5daeeae7404b6fd5d283d7cc3d74a3237a1e9a34
Богдан <brazbogdan@gmail.com>
2025-05-02 16:35:15 +0300

    updated commit

--- flower_shop/src/components/Header/Header.jsx
+++ flower_shop/src/components/Header/Header.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import styles from './Header.module.css';
 import Nav from './Nav/Nav.jsx';
+import Korzyna from './Korzyna/Korzyna.jsx';
 
 export default function Header() {
   return (
     <header className={styles.header}>
       <div className={styles.headerContainer}>
     <Nav></Nav>
+    <Korzyna></Korzyna>
     </div>
     </header>
   )
---
+++ flower_shop/src/components/Header/Korzyna/Korzyna.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './Korzyna.module.css';
+
+export default function Korzyna() {
+  return (
+    <div className={styles.korzyna}>Korzyna</div>
+  )
+}
