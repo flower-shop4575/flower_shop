c7d46eefd6e2da116ae4fe3ef4acf42ef43d83cd
Богдан <brazbogdan@gmail.com>
2025-05-02 17:13:51 +0300

    updated commit

--- flower_shop/src/components/Header/Korzyna/Korzyna.jsx
+++ flower_shop/src/components/Header/Korzyna/Korzyna.jsx
@@ -3,6 +3,12 @@ import styles from './Korzyna.module.css';
 
 export default function Korzyna() {
   return (
-    <div className={styles.korzyna}>Korzyna</div>
-  )
+    <div className={styles.korzyna}>
+      <button className={styles.cartButton}>
+        <img className={styles.cartIcon} src="/korzyna/korzyna.svg" alt="korzyna" />
+        <span className={styles.itemCount}>0</span>
+      </button>
+      <span className={styles.totalPrice}>0 грн</span>
+    </div>
+  );
 }
