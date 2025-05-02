45821fff244a009a088fcd3f20bf9d658e7cc230
Богдан <brazbogdan@gmail.com>
2025-05-02 13:25:08 +0300

    updated commit

--- flower_shop/src/components/Main/Main.jsx
+++ flower_shop/src/components/Main/Main.jsx
@@ -4,7 +4,9 @@ import styles from './Main.module.css';
 export default function Main() {
   return (
     <section className= {styles.mainBanner}>
-        
+        <div className={styles.logoContainer}>
+        <img src="fon/logo.svg" alt="logo" />
+        </div>
     </section>
   )
 }
