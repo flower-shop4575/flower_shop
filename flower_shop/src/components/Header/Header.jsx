c21691503279bc2dfa9f2b61818c4b5a88177aeb
Богдан <brazbogdan@gmail.com>
2025-05-02 11:30:05 +0300

    updated commit

--- flower_shop/src/components/Header/Header.jsx
+++ flower_shop/src/components/Header/Header.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import styles from './Header.module.css';
 import Nav from './Nav/Nav.jsx';
 
 export default function Header() {
   return (
-    <>
+    <header className={styles.header}>
+      <div className={styles.headerContainer}>
     <Nav></Nav>
-    </>
+    </div>
+    </header>
   )
 }
