f2a0efb9ccbe8a86f33e4073ca8dd237dcc91e1f
Богдан <brazbogdan@gmail.com>
2025-05-02 11:52:46 +0300

    updated commit

--- flower_shop/src/components/Header/Nav/Nav.jsx
+++ flower_shop/src/components/Header/Nav/Nav.jsx
@@ -1,7 +1,17 @@
-import React from 'react'
+import React from 'react';
+import styles from './Nav.module.css';
 
 export default function Nav() {
   return (
-    <div>Nav</div>
+    <nav className={styles.nav}>
+      <ul className={styles.navList}>
+        <li className={styles.listItem}><a href="#">Про нас</a></li>
+        <li className={styles.listItem}><a href="#">Доставка</a></li>
+        <li className={styles.listItem}><a href="#">Оплата</a></li>
+        <li className={styles.listItem}><a href="#">Відгуки</a></li>
+        <li className={styles.listItem}><a href="#">Оферта</a></li>
+        <li className={styles.listItem}><a href="#">Контакти</a></li>
+      </ul>
+    </nav>
   )
 }
