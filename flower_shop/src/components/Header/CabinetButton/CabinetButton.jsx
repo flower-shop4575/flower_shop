17d5b5ca3afd1f8f0d0b2336cbed0ab6f2649a9f
Богдан <brazbogdan@gmail.com>
2025-05-02 20:22:40 +0300

    updated commit

--- flower_shop/src/components/Header/CabinetButton/CabinetButton.jsx
+++ flower_shop/src/components/Header/CabinetButton/CabinetButton.jsx
@@ -4,7 +4,9 @@ import styles from './CabinetButton.module.css';
 export default function CabinetButton() {
   return (
     <>
-        <button className= {styles.but}><img src="/control/cabinet.svg" alt="my-cabinet" />Мій кабінет</button>
+        <button className= {styles.but}><img src="/control/cabinet.svg" alt="my-cabinet" />
+        <span className= {styles.span}>Мій кабінет</span>
+        </button>
     </>
   )
 }
