5ba52f12c3beec6cfd85b37a30bee73097f163ea
Богдан <brazbogdan@gmail.com>
2025-05-02 10:55:16 +0300

    updated commit

--- flower_shop/src/App.jsx
+++ flower_shop/src/App.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import './styles/global.css';
-
+import Header from './components/Header/Header.jsx';
 
 function App() {
   
 
   return (
     <>
-      <div>Hello world</div>
+      <Header></Header>
     </>
   )
 }
