e83de4ded546a40752fc975e3aace4f2f0d4d37f
Богдан <brazbogdan@gmail.com>
2025-05-02 12:17:55 +0300

    updated commit

--- flower_shop/src/components/Header/Nav/Nav.jsx
+++ flower_shop/src/components/Header/Nav/Nav.jsx
@@ -5,12 +5,12 @@ export default function Nav() {
   return (
     <nav className={styles.nav}>
       <ul className={styles.navList}>
-        <li className={styles.listItem}><a href="#"><img src='/assets/aboutUs.svg' alt="about" />Про нас</a></li>
-        <li className={styles.listItem}><a href="#"><img src="/assets/car.svg" alt="" />Доставка</a></li>
-        <li className={styles.listItem}><a href="#"><img src="/assets/dollar.svg" alt="" />Оплата</a></li>
-        <li className={styles.listItem}><a href="#"><img src="/assets/smile.svg" alt="" />Відгуки</a></li>
-        <li className={styles.listItem}><a href="#"><img src="/assets/offerta.svg" alt="" />Оферта</a></li>
-        <li className={styles.listItem}><a href="#"><img src="/assets/contacts.svg" alt="" />Контакти</a></li>
+        <li className={styles.listItem}><a className={styles.link} href="#"><img className={styles.icon} src='/assets/aboutUs.svg' alt="about" />Про нас</a></li>
+        <li className={styles.listItem}><a className={styles.link} href="#"><img className={styles.icon} src="/assets/car.svg" alt="" />Доставка</a></li>
+        <li className={styles.listItem}><a className={styles.link} href="#"><img className={styles.icon} src="/assets/dollar.svg" alt="" />Оплата</a></li>
+        <li className={styles.listItem}><a className={styles.link} href="#"><img className={styles.icon} src="/assets/smile.svg" alt="" />Відгуки</a></li>
+        <li className={styles.listItem}><a className={styles.link} href="#"><img className={styles.icon} src="/assets/offerta.svg" alt="" />Оферта</a></li>
+        <li className={styles.listItem}><a className={styles.link} href="#"><img className={styles.icon} src="/assets/contacts.svg" alt="" />Контакти</a></li>
       </ul>
     </nav>
   )
