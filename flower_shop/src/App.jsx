7aa7954da30a916b58ebc6cc7720a8c30ae7dcd3
ByatesEduard <edikbatec1@gmail.com>
2025-05-03 16:29:13 +0300

    03.05 section fixed

--- flower_shop/src/App.jsx
+++ flower_shop/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './styles/global.css';
 import Header from './components/Header/Header.jsx';
 import Main from './components/Main/Main.jsx';
+import {Section} from './components/Section/Section.jsx'
 
 function App() {
   
@@ -10,6 +11,7 @@ function App() {
     <>
       <Header></Header>
       <Main></Main>
+      <Section></Section>
     </>
   )
 }
--- flower_shop/src/components/Section/Section.jsx
+++ flower_shop/src/components/Section/Section.jsx
@@ -4,11 +4,13 @@ import styles from '../Section/Section.module.css';
 export const Section = () => {
   return (
     <section className={styles.section}>
-      <div className={styles.items__1}>Букети </div>
-      <div className={styles.items__1}>Троянди</div>
-      <div className={styles.items__2}>Подарунки</div>
-      <div className={styles.items__2}>Кімнатні рослини</div>
-      <div className={styles.item__3}>Квіти в боксах</div>
+      <div className={styles.content}>
+        <div className={styles.items__1}>Букети </div>
+        <div className={styles.items__1}>Троянди</div>
+        <div className={styles.items__2}>Подарунки</div>
+        <div className={styles.items__2}>Кімнатні рослини</div>
+        <div className={styles.items__3}>Квіти в боксах</div>
+      </div>
     </section>
   )
 }
